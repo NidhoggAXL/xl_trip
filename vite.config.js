9cb2748bf7eac2d4e6cbcecf0ca058b1db24b61f
Draco <2076450855@qq.com>
2025-06-06 20:59:10 +0800

    vant UI 库的组件库使用

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// vant插件使用
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     // vueDevTools(),
+
+    // 使用Vant组件库
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
   resolve: {
     alias: {
