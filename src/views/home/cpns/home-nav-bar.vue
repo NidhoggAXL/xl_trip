5aa11a61bd2dded76cf7c79b57516b461275dcb6
Draco <2076450855@qq.com>
2025-06-07 15:55:45 +0800

    home中home-img图片的设置

---
+++ src/views/home/cpns/home-nav-bar.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="nav-bar">
+    <div class="title">
+      <h1>鸿源旅途</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+  .title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-items: center;
+    font-size: 12px;
+    color: var(--primary-color);
+    border-bottom: 1px solid #eaeaea;
+  }
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -5,9 +5,20 @@ import homeNavBar from './cpns/home-nav-bar.vue';
 <template>
   <div class="home">
     <home-nav-bar />
+
+    <div class="home-img">
+      <img src="@/assets/imgs/home/banner.webp" alt="Home Image" />
+    </div>
   </div>
+
+
 </template>
 
 <style lang="less" scoped>
+  .home-img {
+    img {
+      width: 100%;
+    }
+  }
 
 </style>
