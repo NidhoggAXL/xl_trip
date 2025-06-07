1dc08c46edd46868277b7ae85170dfd8253189a2
Draco <2076450855@qq.com>
2025-06-07 22:29:10 +0800

    city页面搜索框取消按钮的路由设置

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,10 +1,23 @@
 <script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
+const searchValue = ref('')
+const router = useRouter()
+const cancelClick = () => {
+  router.back()
+}
 </script>
 
 <template>
   <div class="city">
-    city
+    <van-search 
+      v-model="searchValue" 
+      placeholder="城市/区域/位置" 
+      show-action
+      shape="round"
+      @cancel="cancelClick"
+    />
   </div>
 </template>
 
