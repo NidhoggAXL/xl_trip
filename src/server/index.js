5cb5e9ef402fdd406342e7299c751898d391b4d3
Draco <2076450855@qq.com>
2025-06-10 16:53:57 +0800

    server中axios的封装

---
+++ src/server/index.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+class AXLRequest {
+  constructor(baseURL, timeout=10000) {
+    this.instance = axios.create({
+      baseURL,
+      timeout,
+    })
+  }
+
+  request(config) {
+    return new Promise((resolve, reject) => {
+      this.instance.request(config)
+        .then(response => {
+          if (response.status === 200) {
+            resolve(response.data);
+          } else {
+            reject(new Error(`Request failed with status code ${response.status}`));
+          }
+        })
+        .catch(error => {
+          if (error.response) {
+            reject(new Error(`Request failed with status code ${error.response.status}: ${error.response.data}`));
+          } else if (error.request) {
+            reject(new Error('No response received from server'));
+          } else {
+            reject(new Error(`Error in request setup: ${error.message}`));
+          }
+        });
+    })
+  }
+
+}
