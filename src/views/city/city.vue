46b1d4aa93acb56a73ff3fdab9c3acad4ea6f785
Draco <2076450855@qq.com>
2025-06-10 19:43:38 +0800

    server文件夹的初始化,city请求测试

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,16 +1,25 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { getCityAll } from '@/server'
 
+// 搜索框的绑定值,使用ref来创建响应式数据
+// 取消按钮的路由跳转
 const searchValue = ref('')
 const router = useRouter()
 const cancelClick = () => {
   router.back()
 }
+
+// 网络请求:请求城市数据
+getCityAll().then(res => {
+  console.log(res)
+})
 </script>
 
 <template>
   <div class="city">
+    <!-- 搜索框的搭建 -->
     <van-search 
       v-model="searchValue" 
       placeholder="城市/区域/位置" 
@@ -18,6 +27,8 @@ const cancelClick = () => {
       shape="round"
       @cancel="cancelClick"
     />
+
+    <!-- tabs的页面搭建 -->
     <van-tabs active="{{ active }}" color="#ff9645">
       <van-tab title="国内·港澳台"></van-tab>
       <van-tab title="海外"></van-tab>
